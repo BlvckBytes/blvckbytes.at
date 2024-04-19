974b6e40ff0158f5f608ad54b4a29a6cfc1fcc17
Philip Hell <blvckbytes@gmail.com>
2024-04-19 18:42:55 +0200

    altered sanitizeForUrl to make for more readable identifiers

--- src/app/utilities.ts
+++ src/app/utilities.ts
@@ -2,5 +2,5 @@ export const sanitizeForUrl = (input: string): string => {
   return input
     .toLowerCase()
     .replaceAll(' ', '-')
-    .replaceAll(/[^A-Za-z0-9-_]/ig, "");
+    .replaceAll(/[^A-Za-z0-9-_]/ig, "-");
 }
